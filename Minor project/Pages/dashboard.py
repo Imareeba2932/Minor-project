eac4a8fe4ef92e265c2671855e8ea306ebc045ac
Areeba Shakeel <shakeelareeba02>
2023-11-04 18:16:53 +0530

    dashboard

--- Minor project/Pages/dashboard.py
+++ Minor project/Pages/dashboard.py
@@ -7,7 +7,7 @@ import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 from plotly.offline import init_notebook_mode
 import seaborn as sns
-import datetime as dt
+import datetime as dt 
 import warnings
 warnings.filterwarnings('ignore')
 pd.set_option('display.max_columns',None)
